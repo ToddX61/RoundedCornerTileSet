134ed128d514657ef9e699a4730560002c02dd5d
Todd Denlinger <todd dot denlinger at biz>
2019-04-17 16:24:02 -0500

    finished initial implementation of Example-iOS

--- Example-iOS/GameViewController.swift
+++ Example-iOS/GameViewController.swift
@@ -6,27 +6,60 @@
 //  Copyright © 2019 Todd. All rights reserved.
 //
 
-import UIKit
-import SpriteKit
 import GameplayKit
+import SpriteKit
+import UIKit
 
 class GameViewController: UIViewController {
+    enum SampleTiles: Int, CaseIterable {
+        case normal = 0, strange = 2
+    }
 
+    private var selected: SampleTiles = .normal
+    private var skScene: SKScene!
+
+    let tileCache = TextureCache(atlasNamed: "RoundedCornerTile.atlas")
+    let TileMapNodeName = "TileMapNode"
+    
+    let tiles = [
+        [0, 0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0, 0],
+    ]
+
+    let strange = [
+        [0, 0, 0, 0, 1, 1, 1, 1, 1],
+        [0, 1, 0, 0, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1],
+        [0, 1, 0, 0, 1, 0, 0, 1, 1],
+        [0, 0, 1, 1, 0, 1, 0, 1, 1],
+        [1, 1, 0, 0, 1, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
+            if let scene = SKScene(fileNamed: "GameScene") as? GameScene {
                 scene.scaleMode = .aspectFill
-                
-                // Present the scene
+                scene.gameSceneDelegate = self
+                skScene = scene
                 view.presentScene(scene)
+                tileCache.load { [weak self] in
+                    guard let self = self else { return }
+                    self.createMap(scene: scene, tiles: self.tiles)
+                }
             }
-            
+
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
@@ -45,6 +78,52 @@ class GameViewController: UIViewController {
     }
 
     override var prefersStatusBarHidden: Bool {
-        return true
+        return false
+    }
+
+    func displaySample() {
+        switch selected {
+        case .normal:
+            createMap(scene: skScene, tiles: strange)
+            selected = .strange
+        case .strange:
+            createMap(scene: skScene, tiles: tiles)
+            selected = .normal
+        }
+    }
+
+    func createMap(scene: SKScene, tiles: [[Int]]) {
+        var grid = Array2D<Any>(columns: 9, rows: 9)
+
+        for (row, rowArray) in tiles.enumerated() {
+            let cellRow = 9 - row - 1
+            for (column, value) in rowArray.enumerated() {
+                grid[column, cellRow] = (value == 0 ? nil : 1)
+            }
+        }
+
+        let tileSet: SKTileSet
+
+        do {
+            tileSet = try RoundedCornerTileSet.create(textures: tileCache)
+        } catch {
+            print(error)
+            return
+        }
+
+        if let node = scene.childNode(withName: TileMapNodeName) {
+            scene.removeChildren(in: [node])
+        }
+        
+        if let map = RoundedCornerTileMapNode.create(tileSet: tileSet, grid: grid) {
+            map.name = TileMapNodeName
+            scene.addChild(map)
+        }
+    }
+}
+
+extension GameViewController: GameSceneDelegate {
+    func touch() {
+        displaySample()
     }
 }
